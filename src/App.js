ec80ba2cc29e3b69856dc5428b9aae4b0ae63d88
pawa kumarvsingh <77200147+Pawan77-lala@users.noreply.github.com>
2022-04-01 22:19:51 +0530

    nje

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>"I am learning React. My life is getting better. "</p>
+
+    </div>
+  );
+}
